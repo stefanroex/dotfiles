6c585359cdfa8707d2f7309d4bb43f9ea6c181c4
Stefan Roex <info@stefanroex.nl>
2016-04-25 10:52:44 +0200

    Add kibit to leiningen

--- home/.emacs.d/init/init-clojure.el
+++ home/.emacs.d/init/init-clojure.el
@@ -81,4 +81,6 @@
   (add-to-list 'cljr-magic-require-namespaces
                '("reagent"  . "reagent.core")))
 
+(use-package kibit-helper)
+
 (provide 'init-clojure)
---
+++ home/.lein/profiles.clj
@@ -0,0 +1,2 @@
+{:user
+ {:plugins [[lein-kibit "0.1.2"]]}}
