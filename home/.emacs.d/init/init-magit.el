a054d7a4b270296ea86aa014af0e613707959d85
Stefan Roex <stefanroex@gmail.com>
2017-04-27 00:00:09 +0200

    Update magit keybindings

--- home/.emacs.d/init/init-magit.el
+++ home/.emacs.d/init/init-magit.el
@@ -1,17 +1,24 @@
 (use-package magit
   :defer t
   :init
+  (keys-l "gs" 'magit-status
+          "gl" 'magit-log
+          "gb" 'magit-blame)
+  :config
   (setq magit-display-buffer-function 'magit-buffer-full-screen)
   (evil-add-hjkl-bindings magit-commit-mode-map 'emacs)
   (evil-add-hjkl-bindings magit-log-mode-map 'emacs)
   (evil-add-hjkl-bindings magit-process-mode-map 'emacs)
   (evil-add-hjkl-bindings magit-status-mode-map 'emacs)
-  (keys :states nil
-        :keymaps 'magit-file-section-map
+  (add-hook 'git-commit-mode-hook 'evil-insert-state)
+
+  (keys :keymaps 'magit-status-mode-map
+        "TAB" 'magit-section-toggle
         "K" 'magit-discard)
 
-  (keys-l "gs" 'magit-status
-          "gl" 'magit-log
-          "gb" 'magit-blame))
+  (keys :keymaps 'magit-blame-mode-map
+        "q" 'magit-blame-quit)
+
+  (global-auto-revert-mode t))
 
 (provide 'init-magit)
