ff12e0ab2a51d8b8129109bf3c05e3f463df8815
Stefan Roex <stefanroex@gmail.com>
2017-04-27 23:07:14 +0200

    Update auto-gen config emacs

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -5,7 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))))
+    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
+ '(package-selected-packages
+   (quote
+    (reveal-in-osx-finder dired+ yaml-mode which-key web-mode use-package smex smart-mode-line slim-mode scss-mode sass-mode project-explorer markdown-mode magit kibit-helper ivy-hydra indent-guide helm-projectile general flycheck flx exec-path-from-shell evil-surround evil-snipe evil-search-highlight-persist evil-nerd-commenter evil-cleverparens elm-mode counsel-projectile company-flow clojure-mode-extra-font-locking clj-refactor challenger-deep-theme better-defaults base16-theme avy alchemist aggressive-indent ag ace-jump-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -14,7 +17,7 @@
  '(evil-search-highlight-persist-highlight-face ((t (:background "#fff" :foreground "#333"))))
  '(isearch ((t (:background "#fff" :foreground "#555"))))
  '(isearch-fail ((t (:background "#fff" :foreground "red"))))
- '(lazy-highlight ((t (:background "#fff" :foreground "#333")))))
+ '(lazy-highlight ((((class color) (min-colors 257)) (:background "#91ddff" :foreground "#100e23")) (((class color) (min-colors 89)) (:background "#87d7ff" :foreground "#121212")))))
 
 (require 'package)
 
