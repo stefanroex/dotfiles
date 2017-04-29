16c475f8a4c06fdbb96937aaca607328fde5a6af
Stefan Roex <stefanroex@gmail.com>
2017-04-29 11:21:36 +0200

    Improve the use of use-package with evil

--- home/.emacs.d/init/init-evil.el
+++ home/.emacs.d/init/init-evil.el
@@ -1,10 +1,8 @@
 (use-package evil
-  :init
+  :config
   (setq evil-intercept-esc 'always
         evil-want-fine-undo t
         evil-shift-width 2)
-  :config
-  (evil-mode t)
   (keys :states nil
         :keymaps '(minibuffer-local-map
                    minibuffer-local-ns-map
@@ -14,31 +12,36 @@
                    ivy-minibuffer-map)
         [escape] 'minibuffer-keyboard-quit)
 
-  (evil-add-hjkl-bindings dired-mode-map 'emacs)
-  (evil-add-hjkl-bindings ibuffer-mode-map 'emacs)
+  (evil-add-hjkl-bindings 'dired-mode-map 'emacs)
+  (evil-add-hjkl-bindings 'ibuffer-mode-map 'emacs)
+
+  (keys :states 'emacs
+        :keymap 'ibuffer-mode-map
+        "J" 'ibuffer-jump-to-buffer)
 
   (keys "C-n" 'next-error
         "C-p" 'previous-error
         "C-h" 'evil-window-left
         "C-j" 'evil-window-down
         "C-k" 'evil-window-up
-        "C-l" 'evil-window-right))
+        "C-l" 'evil-window-right)
+
+  (evil-mode t))
 
 (use-package evil-surround
   :config
   (global-evil-surround-mode t))
 
 (use-package evil-nerd-commenter
-  :defer t
+  :commands 'evilnc-comment-or-uncomment-lines
   :init
   (keys-l
    :states '(normal visual)
    "cc" 'evilnc-comment-or-uncomment-lines))
 
 (use-package evil-search-highlight-persist
-  :init
-  (global-evil-search-highlight-persist t)
   :config
+  (global-evil-search-highlight-persist t)
   (keys :keymaps 'prog-mode-map
         "RET" 'evil-search-highlight-persist-remove-all))
 
