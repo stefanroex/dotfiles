a4e27fa7052aec0e49edfcd0962e6429ff20b052
Stefan Roex <stefanroex@gmail.com>
2018-11-24 15:58:34 +0100

    Add lein nvm and memory-meter to profiles.clj

--- roles/development/clojure/files/lein/profiles.clj
+++ roles/development/clojure/files/lein/profiles.clj
@@ -1,4 +1,7 @@
 {:user {:plugins [[lein-ancient "0.6.14"]
-                  [lein-pprint "1.1.2"]]
-        :injections [(require 'sc.api)]
-        :dependencies [[vvvvalvalval/scope-capture "0.3.1"] ]}}
+                  [lein-pprint "1.1.2"]
+                  [lein-nvd "0.5.6"]]
+        :injections [(require 'sc.api)
+                     (require 'clojure.pprint)]
+        :dependencies [[vvvvalvalval/scope-capture "0.3.2"]
+                       [com.clojure-goes-fast/clj-memory-meter "0.1.2"]]}}
