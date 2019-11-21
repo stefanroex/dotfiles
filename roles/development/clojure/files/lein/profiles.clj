ac936e112ebbcafb340dcff15cfda977487d1189
Stefan Roex <stefanroex@gmail.com>
2019-11-21 23:56:48 +0100

    Update leiningen plugins

--- roles/development/clojure/files/lein/profiles.clj
+++ roles/development/clojure/files/lein/profiles.clj
@@ -1,13 +1,12 @@
-{:user {:plugins [[lein-ancient "0.6.14"]
-                  [lein-pprint "1.1.2"]
-                  [lein-nvd "0.5.6"]
-                  [cider/cider-nrepl "0.21.1"]
-                  [refactor-nrepl "2.4.0"]]
+{:user {:plugins [[lein-ancient "0.6.15"]
+                  [lein-pprint "1.2.0"]
+                  [lein-nvd "1.2.0"]
+                  [cider/cider-nrepl "0.22.4"]]
         :jvm-opts ["-Djdk.attach.allowAttachSelf"]
         :middleware [refactor-nrepl.plugin/middleware
                      cider-nrepl.plugin/middleware]
         :injections [(require 'sc.api)
                      (require 'clojure.pprint)]
         :dependencies [[vvvvalvalval/scope-capture "0.3.2"]
-                       [com.clojure-goes-fast/clj-async-profiler "0.3.0"]
+                       [com.clojure-goes-fast/clj-async-profiler "0.4.0"]
                        [com.clojure-goes-fast/clj-memory-meter "0.1.2"]]}}
