b35e5f057a668b01d37bc5bad1edf92d4cd814d0
Stefan Roex <stefanroex@gmail.com>
2020-04-29 16:14:28 +0200

    Add firefox user-overrides.js

---
+++ roles/applications/files/firefox/user-overrides.js
@@ -0,0 +1,14 @@
+user_pref("browser.display.use_document_fonts", 1);
+user_pref("browser.download.useDownloadDir", true);
+user_pref("browser.search.suggest.enabled", true);
+user_pref("browser.sessionstore.privacy_level", 1);
+user_pref("dom.allow_cut_copy", true);
+user_pref("keyword.enabled", true);
+user_pref("places.history.enabled", true);
+user_pref("privacy.clearOnShutdown.cache", false);
+user_pref("privacy.clearOnShutdown.cookies", false);
+user_pref("privacy.clearOnShutdown.downloads", false);
+user_pref("privacy.clearOnShutdown.history", false);
+user_pref("privacy.clearOnShutdown.offlineApps", false);
+user_pref("privacy.clearOnShutdown.sessions", false);
+user_pref("privacy.resistFingerprinting.letterboxing", false);
