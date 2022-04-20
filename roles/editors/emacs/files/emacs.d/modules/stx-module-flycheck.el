9817e5cde46d6daf3f4ecb7ac3aed93fe18b6587
Stefan Roex <stefanroex@gmail.com>
2022-04-20 17:16:55 +0200

    Extract constant for flycheck ignore

--- roles/editors/emacs/files/emacs.d/modules/stx-module-flycheck.el
+++ roles/editors/emacs/files/emacs.d/modules/stx-module-flycheck.el
@@ -1,13 +1,15 @@
+(defconst stx/flycheck-disabled-checkers
+  '(emacs-lisp-checkdoc
+    ruby-reek
+    javascript-jshint
+    handlebars
+    scss-lint
+    scss))
+
 (use-package flycheck
   :hook (prog-mode . flycheck-mode)
   :config
-  (setq-default flycheck-disabled-checkers
-                '(emacs-lisp-checkdoc
-                  ruby-reek
-                  javascript-jshint
-                  handlebars
-                  scss-lint
-                  scss))
+  (setq-default flycheck-disabled-checkers stx/flycheck-disabled-checkers)
   (setq flycheck-idle-change-delay 1
 	flycheck-emacs-lisp-load-path 'inherit
         flycheck-check-syntax-automatically '(save mode-enabled idle-change)))
