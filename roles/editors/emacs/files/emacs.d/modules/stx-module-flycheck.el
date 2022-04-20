f301bc061577947a1d1f4730afdd43652e733fce
Stefan Roex <stefanroex@gmail.com>
2022-04-20 12:08:47 +0200

    Flycheck in prog-mode only

--- roles/editors/emacs/files/emacs.d/modules/stx-module-flycheck.el
+++ roles/editors/emacs/files/emacs.d/modules/stx-module-flycheck.el
@@ -1,5 +1,5 @@
 (use-package flycheck
-  :defer 1
+  :hook (prog-mode . flycheck-mode)
   :config
   (setq-default flycheck-disabled-checkers
                 '(emacs-lisp-checkdoc
@@ -10,7 +10,6 @@
                   scss))
   (setq flycheck-idle-change-delay 1
 	flycheck-emacs-lisp-load-path 'inherit
-        flycheck-check-syntax-automatically '(save mode-enabled idle-change))
-  (global-flycheck-mode))
+        flycheck-check-syntax-automatically '(save mode-enabled idle-change)))
 
 (provide 'stx-module-flycheck)
