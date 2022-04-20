851bf53663c1396aa9ef4f21eddb140809cb5932
Stefan Roex <stefanroex@gmail.com>
2022-04-20 17:17:12 +0200

    Abstract looks for lisp modes

--- roles/editors/emacs/files/emacs.d/modules/stx-module-lisp.el
+++ roles/editors/emacs/files/emacs.d/modules/stx-module-lisp.el
@@ -1,44 +1,39 @@
-(defconst lisp-hooks
+(defconst stx/lisp-mode-hooks
   '(cider-mode-hook
     cider-repl-mode-hook
     clojure-mode-hook
     emacs-lisp-mode-hook
-    lisp-mode-hook
     lisp-interaction-mode-hook
+    lisp-mode-hook
     scheme-mode-hook))
 
+(defun stx/add-lisp-mode-hook (mode)
+  (dolist (hook stx/lisp-mode-hooks)
+    (add-hook hook mode)))
+
 (use-package paredit
-  :init
-  (dolist (mode-hook lisp-hooks)
-    (add-hook mode-hook #'paredit-mode)))
+  :init (stx/add-lisp-mode-hook 'paredit-mode))
 
 (use-package evil-cleverparens
-  :init
-  (setq evil-cleverparens-use-regular-insert t)
-  (dolist (mode-hook lisp-hooks)
-    (add-hook mode-hook #'evil-cleverparens-mode)))
-
-(use-package aggressive-indent
-  :diminish aggressive-indent-mode
-  :init
-  (dolist (mode-hook '(clojure-mode-hook emacs-lisp-mode-hook))
-    (add-hook mode-hook #'aggressive-indent-mode)))
-
-(use-package eldoc
-  :diminish eldoc-mode
-  :init
-  (dolist (mode-hook '(cider-mode-hook emacs-lisp-mode-hook))
-    (add-hook mode-hook #'eldoc-mode)))
+  :init (stx/add-lisp-mode-hook 'evil-cleverparens-mode)
+  :custom
+  (evil-cleverparens-use-regular-insert t))
 
 (use-package paxedit
-  :init
-  (dolist (mode-hook lisp-hooks)
-    (add-hook mode-hook #'paxedit-mode))
-  :config
+  :init (stx/add-lisp-mode-hook 'paxedit-mode)
+  :general
   (keys
     :states 'normal
     :keymaps 'override
     "M-j" 'paxedit-transpose-backward
     "M-k" 'paxedit-transpose-forward))
+
+(use-package aggressive-indent
+  :diminish aggressive-indent-mode
+  :hook ((clojure-mode emacs-lisp-mode) . aggressive-indent-mode))
+
+(use-package eldoc
+  :diminish eldoc-mode
+  :hook ((cider-mode emacs-lisp-mode) . eldoc-mode))
 
 (provide 'stx-module-lisp)
