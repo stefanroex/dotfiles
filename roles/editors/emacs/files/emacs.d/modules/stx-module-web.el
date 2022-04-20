5bec28fa18dfd92b3e2f339720dc2a357ee17242
Stefan Roex <stefanroex@gmail.com>
2022-04-20 17:18:50 +0200

    Simplify config for web-mode
    Most package have an autoload, so no need to specify the exact mode.

--- roles/editors/emacs/files/emacs.d/modules/stx-module-web.el
+++ roles/editors/emacs/files/emacs.d/modules/stx-module-web.el
@@ -1,45 +1,32 @@
-(use-package yaml-mode)
-
-(use-package markdown-mode
+(use-package web-mode
+  :mode "\\.js[x]?\\'"
+  :custom
+  (web-mode-enable-auto-quoting nil)
+  (web-mode-markup-indent-offset 2)
+  (web-mode-css-indent-offset 2)
+  (web-mode-code-indent-offset 2)
+  (web-mode-attr-indent-offset 2)
+  (web-mode-indent-style 2)
   :config
-  (add-to-list 'auto-mode-alist '("\\.mdx\\'" . markdown-mode)))
+  (setq web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'")))
+  (keys-l :keymaps 'web-mode-map "d" 'flow-type)
+  ;; (add-to-list 'company-dabbrev-code-modes 'web-mode)
+  )
 
 (use-package prettier-js
-  :config
-  (add-hook 'markdown-mode-hook 'prettier-js-mode)
-  (add-hook 'css-mode-hook 'prettier-js-mode)
-  (add-hook 'web-mode-hook 'prettier-js-mode))
+  :hook
+  ((markdown-mode css-mode web-mode) . prettier-js-mode))
 
-(use-package web-mode
-  :mode (("\\.js$" . web-mode))
-  :config
-  (setq web-mode-enable-auto-quoting nil
-        web-mode-markup-indent-offset 2
-        web-mode-css-indent-offset 2
-        web-mode-code-indent-offset 2
-        web-mode-attr-indent-offset 2
-        web-mode-indent-style 2)
+(require 'web-mode)
 
-  (keys-l :keymaps 'web-mode-map
-    "d" 'flow-type)
+(use-package css-mode)
 
-  (add-to-list 'company-dabbrev-code-modes 'web-mode)
+(use-package sass-mode)
 
-  (add-hook 'web-mode-hook
-            (lambda ()
-              (if (equal web-mode-content-type "javascript")
-                  (web-mode-set-content-type "jsx")
-                (message "now set to: %s" web-mode-content-type)))))
+(use-package scss-mode)
 
-(use-package css-mode
-  :mode "\\.css\\'"
-  :config
-  (setq css-indent-offset 2))
+(use-package yaml-mode)
 
-(use-package sass-mode
-  :mode "\\.sass\\'")
-
-(use-package scss-mode
-  :mode "\\.scss\\'")
+(use-package markdown-mode)
 
 (provide 'stx-module-web)
