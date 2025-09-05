c8326b89e3390cf4fdf3c119c9f810423f9f0c95
Stefan Roex <stefanroex@gmail.com>
2025-09-05 11:03:21 +0200

    Improve consult and embark config

--- roles/editors/emacs/files/emacs.d/modules/stx-module-completion.el
+++ roles/editors/emacs/files/emacs.d/modules/stx-module-completion.el
@@ -1,18 +1,33 @@
+(defun consult-ripgrep-at-point ()
+  (interactive)
+  (consult-ripgrep (projectile-project-root) (thing-at-point 'symbol)))
+
 (use-package vertico
   :demand t
-  :init
-  (vertico-mode +1)
-  (setq vertico-cycle t)
+
+  :general
   (keys-l
     "f" 'projectile-find-file
-    "F" 'find-file))
+    "F" 'find-file)
+
+  :custom
+  (vertico-cycle t)
+
+  :init
+  (vertico-mode +1))
 
 (use-package consult
   :general
   (keys-l
     "B" 'consult-buffer
     "b" 'consult-project-buffer
-    "y" 'consult-yank-pop))
+    "y" 'consult-yank-pop
+    "i" 'consult-outline
+    "s" 'consult-ripgrep
+    "S" 'consult-ripgrep-at-point)
+  :custom
+  (xref-show-xrefs-function #'consult-xref)
+  (xref-show-definitions-function #'consult-xref))
 
 (use-package hotfuzz
   :demand t
@@ -26,5 +41,17 @@
 (use-package savehist
   :init
   (savehist-mode))
+
+(use-package embark
+  :general
+  (keys
+    :states '(nil normal emacs motion)
+    "C-;" 'embark-act))
+
+(use-package embark-consult
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
+
+(use-package wgrep)
 
 (provide 'stx-module-completion)
--- roles/editors/emacs/files/emacs.d/modules/stx-module-search.el
+++ roles/editors/emacs/files/emacs.d/modules/stx-module-search.el
@@ -1,23 +1,23 @@
-(use-package rg
-  :general
-  (keys-l
-    "s" 'stx/rg
-    "S" 'stx/rg-literal)
-  :custom
-  (rg-show-header nil)
-  :custom-face
-  (rg-match-face ((t (:inherit highlight))))
-  :config
-  (keys
-    :keymaps 'rg-mode-map
-    "C-n" 'next-error
-    "C-p" 'previous-error)
-  (rg-define-search stx/rg
-    :dir project
-    :files "everything")
-  (rg-define-search stx/rg-literal
-    :dir project
-    :format literal
-    :files "everything"))
+;; (use-package rg
+;;   :general
+;;   (keys-l
+;;     "s" 'stx/rg
+;;     "S" 'stx/rg-literal)
+;;   :custom
+;;   (rg-show-header nil)
+;;   :custom-face
+;;   (rg-match-face ((t (:inherit highlight))))
+;;   :config
+;;   (keys
+;;     :keymaps 'rg-mode-map
+;;     "C-n" 'next-error
+;;     "C-p" 'previous-error)
+;;   (rg-define-search stx/rg
+;;     :dir project
+;;     :files "everything")
+;;   (rg-define-search stx/rg-literal
+;;     :dir project
+;;     :format literal
+;;     :files "everything"))
 
 (provide 'stx-module-search)
