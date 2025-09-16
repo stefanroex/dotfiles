1b002548419e4941e8273ef068faadf0e20f87c1
Stefan Roex <stefanroex@gmail.com>
2025-09-16 22:08:44 +0200

    Use non interactive shell for faster exec-path-from-shell

--- roles/editors/emacs/files/emacs.d/core/stx-core-shell.el
+++ roles/editors/emacs/files/emacs.d/core/stx-core-shell.el
@@ -2,6 +2,7 @@
   :demand t
   :custom
   (exec-path-from-shell-variables '("PATH" "MANPATH" "LSP_USE_PLISTS"))
+  (exec-path-from-shell-arguments '("-l"))
   :config
   (exec-path-from-shell-initialize))
 
