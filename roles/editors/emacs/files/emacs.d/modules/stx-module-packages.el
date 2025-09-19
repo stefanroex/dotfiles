9ff413b4b311f8e734bb4712edbe535803417b72
Stefan Roex <stefanroex@gmail.com>
2025-09-19 08:22:24 +0200

    Enable lsp-mode and formatting in terraform mode

--- roles/editors/emacs/files/emacs.d/modules/stx-module-packages.el
+++ roles/editors/emacs/files/emacs.d/modules/stx-module-packages.el
@@ -1,6 +1,10 @@
 ;;;  -*- lexical-binding: t -*-
 
-(use-package terraform-mode)
+(use-package terraform-mode
+  :hook
+  (terraform-mode . lsp-deferred)
+  :custom
+  (terraform-format-on-save t))
 
 (use-package iflipb
   :custom
