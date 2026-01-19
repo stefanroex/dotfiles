81927755abcc2dfa174ab61e18de4a764ab47fc3
Stefan Roex <stefanroex@gmail.com>
2026-01-19 17:04:56 +0100

    Fix neovim issues

--- roles/editors/vim/files/lua/plugins/treesitter.lua
+++ roles/editors/vim/files/lua/plugins/treesitter.lua
@@ -34,9 +34,6 @@ return {
     indent = { enable = true },  
   },
   config = function(_, opts)
-    if type(opts.ensure_installed) == "table" then
-      opts.ensure_installed = opts.ensure_installed
-    end
-    require("nvim-treesitter.configs").setup(opts)
+    require("nvim-treesitter").setup(opts)
   end,
 }
